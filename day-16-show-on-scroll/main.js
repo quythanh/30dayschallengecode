8319773ebab4d28e8d2b44853b5d92715eae9f92
Quy Thanh <quythanh2205@gmail.com>
2022-04-15 00:09:22 +0700

    add day 16

---
+++ day-16-show-on-scroll/main.js
@@ -0,0 +1,16 @@
+function toggleElement(e) {
+    var elementRect = e.getClientRects()[0],
+        screenHeight = window.innerHeight || document.documentElement.clientHeight
+    
+    (elementRect.bottom < 0 || elementRect.top > screenHeight)
+        ? e.classList.remove('start')
+        : e.classList.add('start')
+}
+
+function checkAnimation() {
+    document.querySelectorAll(".show-on-scroll").forEach(e => toggleElement(e))
+}
+
+window.onscroll = checkAnimation
+
+checkAnimation()
